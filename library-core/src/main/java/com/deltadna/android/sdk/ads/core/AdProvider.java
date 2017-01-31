34b486b187968de9129f94ae6620d88058dbf561
Martin Bella <martin.bella@deltadna.com>
2017-01-31 11:01:54 +0000

    hidden applovin

--- library-core/src/main/java/com/deltadna/android/sdk/ads/core/AdProvider.java
+++ library-core/src/main/java/com/deltadna/android/sdk/ads/core/AdProvider.java
@@ -23,8 +23,6 @@ import com.deltadna.android.sdk.ads.core.network.DummyAdapter;
 import com.deltadna.android.sdk.ads.provider.adcolony.AdColonyAdapter;
 import com.deltadna.android.sdk.ads.provider.admob.AdMobAdapter;
 import com.deltadna.android.sdk.ads.provider.amazon.AmazonAdapter;
-import com.deltadna.android.sdk.ads.provider.applovin.AppLovinInterstitialAdapter;
-import com.deltadna.android.sdk.ads.provider.applovin.AppLovinRewardedAdapter;
 import com.deltadna.android.sdk.ads.provider.chartboost.ChartBoostInterstitialAdapter;
 import com.deltadna.android.sdk.ads.provider.chartboost.ChartBoostRewardedAdapter;
 import com.deltadna.android.sdk.ads.provider.flurry.FlurryInterstitialAdapter;
@@ -95,50 +93,6 @@ enum AdProvider {
                     index,
                     config.getString("appKey"),
                     config.optBoolean("testMode", false));
-        }
-    },
-    
-    APPLOVIN("com.deltadna.android.sdk.ads.provider.applovin.AppLovinInterstitialAdapter") {
-        @Override
-        MediationAdapter createAdapter(
-                int eCPM,
-                int adFloorPrice,
-                int demoteOnCode,
-                int index,
-                JSONObject config) throws JSONException {
-            
-            return new AppLovinInterstitialAdapter(
-                    eCPM,
-                    demoteOnCode,
-                    index,
-                    config.getString("key"),
-                    config.optBoolean("verboseLogging", false),
-                    config.optLong("adRefreshSeconds", -1));
-        }
-        
-        @Nullable
-        @Override
-        AdProvider rewarded() {
-            return APPLOVIN_REWARDED;
-        }
-    },
-    
-    APPLOVIN_REWARDED(APPLOVIN.cls) {
-        @Override
-        MediationAdapter createAdapter(
-                int eCPM,
-                int adFloorPrice,
-                int demoteOnCode,
-                int index,
-                JSONObject config) throws JSONException {
-            
-            return new AppLovinRewardedAdapter(
-                    eCPM,
-                    demoteOnCode,
-                    index,
-                    config.getString("key"),
-                    config.optBoolean("verboseLogging", false),
-                    config.optLong("adRefreshSeconds"));
         }
     },
     
